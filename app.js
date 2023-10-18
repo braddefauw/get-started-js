43ea914aa9b0004fbc52f2a3139ca135d10a8ac9
Brad DeFauw <badefauw@gmail.com>
2023-10-18 17:10:13 -0500

    create a sqlite database, recipe model, synchronize

--- app.js
+++ app.js
@@ -7,6 +7,31 @@ const Sequelize = require('sequelize');
 const app = express()
 app.use(bodyParser.json())
 
+// Create a SQLite database using Sequelize
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: 'recipes.db',
+});
+  
+// Define a Recipe model
+const Recipe = sequelize.define('recipe', {
+    name: { type: Sequelize.STRING, allowNull: false },
+    ingredients: Sequelize.STRING,
+    instructions: Sequelize.TEXT,
+});
+  
+  // Synchronize the model with the database
+sequelize.sync().then(() => {
+    console.log('Database & tables created!');
+});
+
+// Middleware to handle errors
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+  
+
 // start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
